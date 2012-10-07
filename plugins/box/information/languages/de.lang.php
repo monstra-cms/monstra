13675b1f3c263907797ff87eb0a0aec749ae9fdb
Yannick Albert <mail@yckart.com>
2012-10-07 20:48:11 +0200

    added german translation

---
+++ plugins/box/information/languages/de.lang.php
@@ -0,0 +1,33 @@
+<?php
+
+    return array(
+        'information' => array(
+            'Information' => 'Informationen',
+            'Debuging' => 'Debugging',
+            'Name' => 'Name',
+            'Value' => 'Wert',
+            'Security' => 'Sicherheit',
+            'System' => 'System',
+            'on' => 'An',
+            'off'=> 'Aus',
+            'Server' => 'Server',
+            'PHP version' => 'PHP Version',
+            'SimpleXML module' => 'SimpleXML Modul',
+            'DOM module' => 'DOM Modul',
+            'Installed' => 'Installiert',
+            'Not Installed' => 'Nicht Installiert',
+            'System version' => 'System Version',
+            'System version ID' => 'System Versions ID',
+            'Security check results' => 'Sicherheits-Check Ergebnisse',
+            'The configuration file has been found to be writable. We would advise you to remove all write permissions on defines.php on production systems.' =>
+            'Die Konfigurationsdatei wurde als beschreibbar entdeckt. Wir empfehlen alle Schreibrechte für defines.php auf Produktionssystemen zu entfernen.',
+            'The Monstra core directory (":path") and/or files underneath it has been found to be writable. We would advise you to remove all write permissions. <br/>You can do this on unix systems with: <code>chmod -R a-w :path</code>' =>
+            'Das Monstra-Core-Verzeichnis (":path") und/oder Dateien unterhalb des Verzeichnisses wurden als beschreibbar entdeckt. Wir empfehlen alle Schreibrechte zu entfernen. <br/>Du kannst dies auf Unix-Systemen ganz einfach mit: <code>chmod -R a-w :path</code> erledigen.',
+            'The Monstra .htaccess file has been found to be writable. We would advise you to remove all write permissions. <br/>You can do this on unix systems with: <code>chmod a-w :path</code>' =>
+            'Die Monstra .htaccess Datei wurde als beschreibbar entdeckt. Wir empfehlen alle Schreibrechte zu entfernen. <br/>Du kannst dies auf Unix-Systemen ganz einfach mit: <code>chmod a-w :path</code> erledigen.',
+            'The Monstra index.php file has been found to be writable. We would advise you to remove all write permissions. <br/>You can do this on unix systems with: <code>chmod a-w :path</code>' =>
+            'Die Monstra index.php Datei wurde als beschreibbar entdeckt. Wir empfehlen alle Schreibrechte zu entfernen. <br/>Du kannst dies auf Unix-Systemen ganz einfach mit: <code>chmod a-w :path</code> erledigen.',
+            'Due to the type and amount of information an error might give intruders when Core::$environment = Core::DEVELOPMENT, we strongly advise setting Core::PRODUCTION in production systems.' =>
+            'Aufgrund der Art und Menge der Informationen kann ein Fehler Eindringlinge ergeben wenn Core::$environment = Core::DEVELOPMENT, wir raten dringend dazu folgendermaßen Core::PRODUCTION in Produktionssystemen einzustellen.',
+        )
+    );
---
+++ plugins/box/plugins/languages/de.lang.php
@@ -0,0 +1,20 @@
+<?php
+
+    return array(
+        'plugins' => array(
+            'Plugins' => 'Plugins',
+            'Name' => 'Name',
+            'Actions' => 'Aktionen',
+            'Description' => 'Beschreibung',
+            'Installed' => 'Installiert',
+            'Install New' => 'Installiere Neue',
+            'Delete' => 'Löschen',
+            'Delete plugin :plugin' => 'Lösche Plugin :plugin',
+            'This plugins does not exist' => 'Diese Plugins existiert nicht',
+            'Version' => 'Version',
+            'Author' => 'Author',
+            'Get More Plugins' => 'Hole Weitere Plugins',
+            'Install' => 'Installieren',
+            'Uninstall' => 'Deinstallieren',
+        )
+    );
