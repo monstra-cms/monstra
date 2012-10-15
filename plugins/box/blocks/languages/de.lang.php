c6cdf8e75c94823d520dde2dbf32e2f481733673
Martynas Barzda <martynas@barzda.info>
2012-10-16 01:00:38 +0300

    Added de, it translations

---
+++ plugins/box/blocks/languages/de.lang.php
@@ -0,0 +1,25 @@
+<?php
+
+    return array(
+        'blocks' => array(
+            'Blocks' => 'Blöcke',
+            'Blocks manager' => 'Block-Manager',
+            'Delete' => 'Löschen',
+            'Edit' => 'Bearbeiten',
+            'Name' => 'Name',
+            'Create new block' => 'Erstelle einen neuen Block',
+            'New block' => 'Neuer Block',
+            'Edit block' => 'Bearbeite Block',
+            'Save' => 'Save',
+            'Save and exit' => 'Speichern und Schließen',
+            'Actions' => 'Aktionen',
+            'Required field' => 'Erforderliches Feld',
+            'This block already exists' => 'Dieser Block existiert bereits',
+            'This block does not exist' => 'Dieser Block existiert nicht',
+            'Delete block: :block' => 'Lösche Block: :block',
+            'Block content' => 'Block-Inhalt',
+            'Block <i>:name</i> deleted' => 'Block <i>:name</i> gelöscht',
+            'Your changes to the block <i>:name</i> have been saved.' => 'Deine Änderungen für den Block <i>:name</i> wurden übernommen.',
+            'Delete block: :block' => 'Lösche Block: :block',
+        )
+    );
---
+++ plugins/captcha/languages/it.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+        'captcha' => array(
+            'Captcha' => 'Captcha',
+            'Captcha plugin for Monstra' => 'Captcha plugin per Monstra',
+            'Captcha code is wrong' => 'Codice captcha è errato',
+        ) 
+    );
