13ae0b1c6af7c46c88d7c7edb2dec729d9109df9
Awilum <awilum@msn.com>
2012-10-21 20:17:17 +0300

    Localization: pt-br translates added.

---
+++ plugins/captcha/languages/pt-br.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+        'captcha' => array(
+            'Captcha' => 'Captcha',
+            'Captcha plugin for Monstra' => 'Plugin de captcha para o Monstra CMS',
+            'Captcha code is wrong' => 'O Captcha está errado',
+        ) 
+    );
