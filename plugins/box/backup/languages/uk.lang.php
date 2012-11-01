9ad69382803cbd9c9dd8c5f183ae131528986e16
Awilum <awilum@msn.com>
2012-11-01 19:55:20 +0200

    Localization: UK translations added #54

---
+++ plugins/box/backup/languages/uk.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    return array(
+        'backup' => array(
+            'Backups' => 'Бекапи',
+            'Backup date' => 'Бекап',
+            'Create backup' => 'Зробити бекап',
+            'Delete' => 'Видалити',
+            'storage' => 'дані',
+            'public' => 'публічна',
+            'plugins' => 'плагіни', 
+            'Size' => 'Розмір',       
+            'Actions' => 'Дії',
+            'Delete backup: :backup' => 'Видалити бекап: :backup',
+            'Creating...' => 'Створення...',
+        )
+    );
---
+++ plugins/box/blocks/languages/uk.lang.php
@@ -0,0 +1,25 @@
+<?php
+
+    return array(
+        'blocks' => array(
+            'Blocks' => 'Блоки',
+            'Blocks manager' => 'Менеджер блоків',  
+            'Delete' => 'Видалити',
+            'Edit' => 'Редагувати',
+            'New block' => 'Новий блок',  
+            'Create new block' => 'Створити новий блок', 
+            'Name' => 'Назва',
+            'Edit block' => 'Редагування блоку',
+            'Save' => 'Зберегти',    
+            'Actions' => 'Дії',
+            'Save and exit' => 'Зберегти та вийти',        
+            'Required field' => 'Обов&prime;язкове поле',
+            'This block already exists' => 'Такий блок вже існує',
+            'This block does not exist' => 'Такого блоку не існує',
+            'Delete block: :block' => 'Видалити блок: :block',
+            'Block content' => 'Вміст блоку',
+            'Block <i>:name</i> deleted' => 'Сніппет <i>:name</i> вилучен',
+            'Your changes to the block <i>:name</i> have been saved.' => 'Ваші зміни до блоку <i>:name</i> були збережені.',
+            'Delete block: :block' => 'Видалити блок: :block',
+        )
+    ); 
