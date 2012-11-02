99862efe2e3843e38eefda3d726cf682517d7ab6
Awilum <awilum@msn.com>
2012-11-02 23:18:37 +0200

    Information Plugin: add "Directory Permissions" tab.

--- plugins/box/information/languages/uk.lang.php
+++ plugins/box/information/languages/uk.lang.php
@@ -16,8 +16,6 @@
             'DOM module' => 'DOM модуль',
             'Installed' => 'Встановлено',
             'Not Installed' => 'Не встановлено',
-            'System version' => 'Версія системы',
-            'System version ID' => 'Версия системы ID',
             'Security check results' => 'Результати перевірки безпеки',
             'The configuration file has been found to be writable. We would advise you to remove all write permissions on defines.php on production systems.' => 
             'Конфігураційний файл доступен для запису. Ми рекомендуємо вам видалити права запису на файл defines.php на живому сайті.',
@@ -29,5 +27,11 @@
             'Головный index.php файл доступен для запису. Ми рекомендуємо вам видалити права запису на головний index.php файл. <br/>Ви можете зробити це на UNIX системах так: <code>chmod -R a-w :path</code>',
             'Due to the type and amount of information an error might give intruders when Core::$environment = Core::DEVELOPMENT, we strongly advise setting Core::PRODUCTION in production systems.' =>
             'Система працює в режимі Core::DEVELOPMENT Ми рекомендуємо вам встановити режим Core::PRODUCTION на живому сайті.',
+            'Monstra version' => 'Monstra Версія',
+            'Directory Permissions' => 'Права Доступу',
+            'Directory' => 'Каталог',
+            'Writable' => 'Доступна для запису',
+            'Unwritable' => 'Недоступна для запису',
+            'Status' => 'Статус',
         )
     );
