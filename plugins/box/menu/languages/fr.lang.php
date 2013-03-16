f3c304c5b7beac3528a863b3d95dc8c8d21be9b4
Awilum <awilum@msn.com>
2013-03-16 11:09:58 +0200

    Localization: Français(fr) translations added.

---
+++ plugins/box/menu/languages/fr.lang.php
@@ -0,0 +1,27 @@
+<?php
+
+    return array(
+        'menu' => array(
+            'Menu' => 'Menu',        
+            'Menu manager' => 'Gestionnaire du menu',
+            'Edit' => 'Editer',
+            'Name' => 'Nom',
+            'Delete' => 'Supprimer',
+            'Order' => 'Ordre',
+            'Actions' => 'Actions',
+            'Create new item' => 'Créer un nouvel item',
+            'New item' => 'Nouveau item',
+            'Item name' => 'Nom de l\'item',
+            'Item order' => 'Ordre de l\'item',
+            'Item target' => 'Cible de l\'item',
+            'Item link' => 'Lien de l\'item',
+            'Item category' => 'Catégorie de l\'item',
+            'Save' => 'Enregistrer',
+            'Edit item' => 'Editer l\'item',
+            'Delete item :name' => 'Supprimer l\'item :name',
+            'Select page' => 'Sélectionner la page',
+            'Category' => 'Catégorie',
+            'Select category' => 'Sélectionner une catégorie',
+            'Required field' => 'Champ requis',
+        )
+    );
---
+++ plugins/captcha/languages/fr.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+        'captcha' => array(
+            'Captcha' => 'Captcha',
+            'Captcha plugin for Monstra' => 'Plugin Captcha pour Monstra',
+            'Captcha code is wrong' => 'Le code Captcha est erroné',
+        ) 
+    );
