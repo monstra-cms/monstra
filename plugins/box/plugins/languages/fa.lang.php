a01a304a832c74728eea3a019c7f1f45c772477a
Awilum <awilum@msn.com>
2013-03-22 00:22:19 +0200

    Localization: Persian(fa) translations added.

---
+++ plugins/box/plugins/languages/fa.lang.php
@@ -0,0 +1,20 @@
+﻿<?php
+    
+    return array(
+        'plugins' => array(
+            'Plugins' => 'افزونه ها',
+            'Name' => 'نام',
+            'Actions' => 'عملیات',
+            'Description' => 'توضیحات',
+            'Installed' => 'نصب شده',
+            'Install New' => 'نصب جدید',
+            'Delete' => 'حذف',
+            'Delete plugin :plugin' => 'حذف افزونه :plugin',
+            'This plugin does not exist' => 'این افزونه وجود ندارد',
+            'Version' => 'نسخه',
+            'Author' => 'مولف',
+            'Get More Plugins' => 'دریافت افزونه های بیشتر',
+            'Install' => 'نصب',
+            'Uninstall' => 'حذف',
+        )
+    );
