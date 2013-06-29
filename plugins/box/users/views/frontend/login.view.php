e8c599f33f6fb60c7e9e49e59e13ad4e02ee69ba
Awilum <awilum@msn.com>
2013-06-29 13:44:13 +0300

    Users Plugin: login page fixes

--- plugins/box/users/views/frontend/login.view.php
+++ plugins/box/users/views/frontend/login.view.php
@@ -5,9 +5,5 @@
     <?php echo Form::hidden('csrf', Security::token()); ?>
     <label><?php echo __('Username', 'users'); ?></label><input name="username" type="text" class="input-large" />
     <label><?php echo __('Password', 'users'); ?></label><input name="password" type="password" class="input-large" />
-<<<<<<< HEAD
-    <br /><input name="login_submit" class="btn" type="submit" value="<?php echo __('Log In', 'users'); ?>" /> <a class="small-grey-text reset-password-btn" href="<?php echo Option::get('siteurl').'users/password-reset'; ?>"><?php echo __('Forgot your password?', 'users');?></a></td></tr>
-=======
-    <br /><input name="login_submit" class="btn" type="submit" value="<?php echo __('Login', 'users'); ?>" /> <a class="small-grey-text reset-password-btn" href="<?php echo Option::get('siteurl').'users/password-reset'; ?>"><?php echo __('Forgot your password?', 'users');?></a></td></tr>
->>>>>>> refs/remotes/origin/master
+    <br /><input name="login_submit" class="btn" type="submit" value="<?php echo __('Log In', 'users'); ?>" /> <a class="small-grey-text reset-password-btn" href="<?php echo Option::get('siteurl').'users/password-reset'; ?>"><?php echo __('Forgot your password?', 'users');?></a>
 </form>
