e0b21a9a3f1b7c5585e6cb934338fe86f28834d3
micheldejoode <micheldejoode@gmail.com>
2013-09-19 06:59:57 +0200

    Create nl.lang.php
    Completed dutch translation

---
+++ plugins/box/backup/languages/nl.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    return array(
+        'backup' => array(
+            'Backups' => 'Backups',
+            'Backup' => 'Backup',
+            'Create Backup' => 'Maak backup',
+            'Delete' => 'Wissen',
+            'storage' => 'opslag',
+            'public' => 'Publiekelijk',
+            'plugins' => 'Plugins',
+            'Size' => 'Grootte',
+            'Actions' => 'Acties',
+            'Delete backup: :backup' => 'Wis backup: :backup',
+            'Creating...' => 'Backup wordt gemaakt...',
+        )
+    );
