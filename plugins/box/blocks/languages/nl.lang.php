e6ffd0130e42b8bd2166d38550752d2081780656
micheldejoode <micheldejoode@gmail.com>
2013-09-19 07:06:32 +0200

    Create nl.lang.php
    Completed dutch translation

---
+++ plugins/box/blocks/languages/nl.lang.php
@@ -0,0 +1,29 @@
+<?php
+
+    return array(
+        'blocks' => array(
+            'Blocks' => 'Blokken',
+            'Blocks manager' => 'Blokken beheer',
+            'Delete' => 'Wissen',
+            'Edit' => 'Bewerken',
+            'Name' => 'Naam',
+            'Create New Block' => 'Maak nieuw blok aan',
+            'New Block' => 'Nieuw blok',
+            'Edit Block' => 'Bewerk blok',
+            'Save' => 'Opslaan',
+            'Save and Exit' => 'Opslaan en terug',
+            'Actions' => 'Acties',
+            'Required field' => 'Vereist veld',
+            'This block already exists' => 'Dit blok bestaat al',
+            'This block does not exist' => 'Dit blok bestaat niet',
+            'Delete block: :block' => 'Wis blok: :block',
+            'Block content' => 'Inhoud blok',
+            'Block <i>:name</i> deleted' => 'Blok <i>:name</i> gewist',
+            'Your changes to the block <i>:name</i> have been saved.' => 'De aanpassingen aan blok <i>:name</i> zijn opgeslagen.',
+            'Delete block: :block' => 'Wis blok: :block',
+            'View Embed Code' => 'Bekijk insluit-code',
+            'Embed Code' => 'Insluit-code',
+            'Shortcode' => 'Verkorte-code',
+            'PHP Code' => 'PHP Code',
+        )
+    );
