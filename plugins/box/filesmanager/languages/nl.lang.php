eecbcbc4eaab8f9a036683953b72dae4221fa609
micheldejoode <micheldejoode@gmail.com>
2013-09-19 07:09:52 +0200

    Create nl.lang.php
    Completed dutch translation

---
+++ plugins/box/filesmanager/languages/nl.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    return array(
+        'filesmanager' => array(
+            'Files' => 'Bestanden',
+            'Files manager' => 'Bestandsbeheer',
+            'Name' => 'Naam',
+            'Actions' => 'Acties',
+            'Delete' => 'Wissen',
+            'Upload' => 'Uploaden',
+            'directory' => 'Map',
+            'Delete directory: :dir' => 'Wis map: :dir',
+            'Delete file: :file' => 'Wis bestand: file',
+            'Extension' => 'Uitbreiding',
+            'Size' => 'Grootte',
+        )
+    );
