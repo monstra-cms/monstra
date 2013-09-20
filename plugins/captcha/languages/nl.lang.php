276c4290709e9855fb79e77bb12ba8290e864fa7
micheldejoode <micheldejoode@gmail.com>
2013-09-20 19:34:07 +0200

    Create nl.lang.php

---
+++ plugins/captcha/languages/nl.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+        'captcha' => array(
+            'Captcha' => 'Captcha',
+            'Captcha plugin for Monstra' => 'Captcha Plugin voor Monstra',
+            'Captcha code is wrong' => 'Captcha Code is helaas foutief',
+        )
+    );
