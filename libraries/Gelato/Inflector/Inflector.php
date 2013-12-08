fc2183b9702ecbc75324aa72a67395eb7cd396f9
Awilum <awilum@msn.com>
2013-12-08 20:55:48 +0400

    Set new Copyright date

--- libraries/Gelato/Inflector/Inflector.php
+++ libraries/Gelato/Inflector/Inflector.php
@@ -9,7 +9,7 @@
  * @package     Gelato
  *
  * @author      Romanenko Sergey / Awilum <awilum@msn.com>
- * @copyright   2012-2013 Romanenko Sergey / Awilum <awilum@msn.com>
+ * @copyright   2012-2014 Romanenko Sergey / Awilum <awilum@msn.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
