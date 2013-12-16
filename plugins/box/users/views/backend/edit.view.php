2fbb4c30c3564b1e5a8d0453298527f5e126bc52
Awilum <awilum@msn.com>
2013-12-16 10:03:24 +0200

    Frontend Improvements

--- plugins/box/users/views/backend/edit.view.php
+++ plugins/box/users/views/backend/edit.view.php
@@ -1,4 +1,3 @@
-<!-- Users_edit -->
 <?php
 
     // Show template for exist user else show error
@@ -8,14 +7,13 @@
 
 ?>
 
-<br />
+<br>
 
 <?php if (Notification::get('success')) Alert::success(Notification::get('success')); ?>
 <?php if (Notification::get('error'))   Alert::error(Notification::get('error')); ?>
 
-<div>
-
-    <div class="span7">
+<div class="row">
+    <div class="col-md-6">
     <?php
 
         echo (
@@ -25,54 +23,108 @@
         );
 
         if (isset($_SESSION['user_role']) && in_array($_SESSION['user_role'], array('admin'))) {
+    ?>
+        <div class="form-group">
+    <?php
             echo Form::label('login', __('Username', 'users'));
-            echo Form::input('login', $user['login'], array('class' => 'input-xlarge'));
+            echo Form::input('login', $user['login'], array('class' => 'form-control'));
+    ?>
+        </div>
+    <?php
         } else {
             echo Form::hidden('login', $user['login']);
         }
-
-        echo (
-            Html::br().
+    ?>
+        <div class="form-group">
+    <?php            
+        echo (            
             Form::label('firstname', __('Firstname', 'users')).
-            Form::input('firstname', $user_firstname, array('class' => 'input-xlarge')).Html::br().
-            Form::label('lastname', __('Lastname', 'users')).
-            Form::input('lastname', $user_lastname, array('class' => 'input-xlarge')).Html::br().
-            Form::label('email', __('Email', 'users')).
-            Form::input('email', $user_email, array('class' => 'input-xlarge')).Html::br().
-            Form::label('twitter', __('Twitter', 'users')).
-            Form::input('twitter', $user_twitter, array('class' => 'input-xlarge')).Html::br().
-            Form::label('skype', __('Skype', 'users')).
-            Form::input('skype', $user_skype, array('class' => 'input-xlarge')).Html::br().
-            Form::label('about_me', __('About Me', 'users')).
-            Form::textarea('about_me', $user_about_me, array('class' => 'input-xlarge')).Html::br()
+            Form::input('firstname', $user_firstname, array('class' => 'form-control'))
         );
-
+    ?>
+        </div>
+        <div class="form-group">
+    <?php 
+        echo (
+            Form::label('lastname', __('Lastname', 'users')).
+            Form::input('lastname', $user_lastname, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+        <div class="form-group">
+    <?php
+        echo (
+            Form::label('email', __('Email', 'users')).
+            Form::input('email', $user_email, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+        <div class="form-group">
+    <?php
+        echo (
+            Form::label('twitter', __('Twitter', 'users')).
+            Form::input('twitter', $user_twitter, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+        <div class="form-group">
+    <?php
+        echo (
+            Form::label('skype', __('Skype', 'users')).
+            Form::input('skype', $user_skype, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+        <div class="form-group">
+    <?php 
+        echo (
+            Form::label('about_me', __('About Me', 'users')).
+            Form::textarea('about_me', $user_about_me, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+    <?php
         if (isset($_SESSION['user_role']) && in_array($_SESSION['user_role'], array('admin'))) {
+    ?>
+        <div class="form-group">
+    <?php
             echo Form::label('role', __('Role', 'users'));
-            echo Form::select('role', array('admin' => __('Admin', 'users'), 'editor' => __('Editor', 'users'), 'user' => __('User', 'users')), $user['role'], array('class' => 'input-xlarge')). Html::br();
+            echo Form::select('role', array('admin' => __('Admin', 'users'), 'editor' => __('Editor', 'users'), 'user' => __('User', 'users')), $user['role'], array('class' => 'form-control'))
+    ?>
+        </div>
+    <?php
         } else {
             echo Form::hidden('role', $_SESSION['user_role']);
         }
 
         echo (
             Html::br().
-            Form::submit('edit_profile', __('Save', 'users'), array('class' => 'btn')).
+            Form::submit('edit_profile', __('Save', 'users'), array('class' => 'btn btn-primary')).
             Form::close()
         );
 
     ?>
     </div>
 
-    <div class="span5">
+    <div class="col-md-6">
     <?php
-
         echo (
             Form::open().
             Form::hidden('csrf', Security::token()).
-            Form::hidden('user_id', Request::get('user_id')).
+            Form::hidden('user_id', Request::get('user_id'))
+        );
+    ?>
+        <div class="form-group">
+    <?php
+        echo (
             Form::label('new_password', __('New password', 'users')).
-            Form::password('new_password', null, array('class' => 'input-xlarge')).Html::br().Html::br().
-            Form::submit('edit_profile_password', __('Save', 'users'), array('class' => 'btn')).
+            Form::password('new_password', null, array('class' => 'form-control'))
+        );
+    ?>
+        </div>
+    <?php
+        echo (    
+            Form::submit('edit_profile_password', __('Save', 'users'), array('class' => 'btn btn-primary')).
             Form::close()
         );
     ?>
@@ -80,11 +132,10 @@
 
 </div>
 
-<div style="clear:both"></div>
+<div style="clear:both;"></div>
 
 <?php
     } else {
         echo '<div class="message-error">'.__('This user does not exist', 'users').'</div>';
     }
 ?>
-<!-- /Users_edit -->
