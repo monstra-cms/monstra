83264e41f3fb4919e863b3f95977b16c9ba7788c
Awilum <awilum@msn.com>
2014-01-03 19:07:05 +0200

    Blog Plugin as a part of Monstra CMS

---
+++ plugins/blog/languages/ru.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+    return array(
+        'blog' => array(
+            'Blog' => 'Блог',
+            'Blog plugin for Monstra' => 'Плагин блога для Monstra',
+            'begin' => 'начало',
+            'end' => 'конец',
+            'prev' => 'назад',
+            'next' => 'вперед',
+            'Related posts' => 'Статьи по теме',
+        ) 
+    );
---
+++ plugins/blog/views/frontend/index.view.php
@@ -0,0 +1,5 @@
+<?php foreach($posts as $post) { ?>
+    <a href="<?php echo Option::get('siteurl'); ?><?php echo Blog::$parent_page_name; ?>/<?php echo $post['slug'] ?>"><?php echo $post['title']; ?></a> <small  class="monstra-blog-date"><?php echo Date::format($post['date'], 'd M Y'); ?></small><br>
+    <?php echo $post['content']; ?>
+    <br><br>
+<?php } ?>
