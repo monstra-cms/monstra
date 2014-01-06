2baee12915fa6d5df32bf20ebbd8d25ebe87350a
Awilum <awilum@msn.com>
2014-01-06 22:54:04 +0200

    UI Improvements - Users Client Side - Fixes #194

--- plugins/box/users/views/frontend/password_reset.view.php
+++ plugins/box/users/views/frontend/password_reset.view.php
@@ -4,22 +4,33 @@
 <?php if (Notification::get('success')) Alert::success(Notification::get('success')); ?>
 <?php if (Notification::get('error'))   Alert::success(Notification::get('error')); ?>
 
-<form method="post">
-<?php
-    echo (Form::hidden('csrf', Security::token()));
-?>
+<div class="row">
+	<div class="col-md-3">
+		<form method="post">
 
-<label><?php echo __('Username', 'users'); ?></label><input type="text" value="<?php echo $user_login; ?>" name="login" class="input-large">
-<?php
-    if (isset($errors['users_user_doesnt_exists'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_user_doesnt_exists'].'</span>';
-    if (isset($errors['users_empty_field'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_empty_field'].'</span>';
-?>
+		<?php echo (Form::hidden('csrf', Security::token())); ?>
 
-<?php if (Option::get('captcha_installed') == 'true') { ?>
-<label><?php echo __('Captcha', 'users'); ?></label>
-<input type="text" name="answer" class="input-large"><?php if (isset($errors['users_captcha_wrong'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_captcha_wrong'].'</span>'; ?>
-<?php CryptCaptcha::draw(); ?>
-<?php } ?>
+		<div class="form-group">
+			<label><?php echo __('Username', 'users'); ?></label>
+			<input type="text" value="<?php echo $user_login; ?>" name="login" class="form-control">
+			<?php
+			    if (isset($errors['users_user_doesnt_exists'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_user_doesnt_exists'].'</span>';
+			    if (isset($errors['users_empty_field'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_empty_field'].'</span>';
+			?>
+		</div>
 
-<br><input type="submit" class="btn" value="<?php echo __('Send New Password', 'users'); ?>" name="reset_password_submit">
-</form>
+		<?php if (Option::get('captcha_installed') == 'true') { ?>
+		<div class="form-group">
+			<label><?php echo __('Captcha', 'users'); ?></label>
+			<input type="text" name="answer" class="form-control"><?php if (isset($errors['users_captcha_wrong'])) echo Html::nbsp(3).'<span class="error">'.$errors['users_captcha_wrong'].'</span>'; ?>
+			<br>
+			<?php CryptCaptcha::draw(); ?>
+		</div>
+		<?php } ?>
+
+		<div class="form-group">
+			<input type="submit" class="btn btn-primary" value="<?php echo __('Send New Password', 'users'); ?>" name="reset_password_submit">
+		</div>
+		</form>
+	</div>
+</div>
