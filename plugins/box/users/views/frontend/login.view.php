44860b5cbac2dd0101e401cdc6cae929a7686c83
Awilum <awilum@msn.com>
2014-01-06 22:36:24 +0200

    UI Improvements - Users Client Side - Fixes #194

--- plugins/box/users/views/frontend/login.view.php
+++ plugins/box/users/views/frontend/login.view.php
@@ -1,9 +1,21 @@
 <h3><?php echo __('Log In', 'users'); ?></h3>
 <hr>
 <?php if (Notification::get('error')) Alert::error(Notification::get('error')); ?>
-<form method="post">
-    <?php echo Form::hidden('csrf', Security::token()); ?>
-    <label><?php echo __('Username', 'users'); ?></label><input name="username" type="text" class="input-large" />
-    <label><?php echo __('Password', 'users'); ?></label><input name="password" type="password" class="input-large" />
-    <br><input name="login_submit" class="btn" type="submit" value="<?php echo __('Log In', 'users'); ?>" /> <a class="small-grey-text reset-password-btn" href="<?php echo Option::get('siteurl').'/users/password-reset'; ?>"><?php echo __('Forgot your password?', 'users');?></a>
-</form>
+<div class="row">
+	<div class="col-md-3">
+		<form method="post">
+		    <?php echo Form::hidden('csrf', Security::token()); ?>
+		    <div class="form-group">
+		    	<label><?php echo __('Username', 'users'); ?></label>
+		    	<input name="username" type="text" class="form-control">
+		    </div>    
+		    <div class="form-group">
+			    <label><?php echo __('Password', 'users'); ?></label>
+			    <input name="password" type="password" class="form-control">
+			</div>
+			<div class="form-group">
+				<input name="login_submit" class="btn btn-primary" type="submit" value="<?php echo __('Log In', 'users'); ?>"> <a class="small-grey-text reset-password-btn" href="<?php echo Option::get('siteurl').'/users/password-reset'; ?>"><?php echo __('Forgot your password?', 'users');?></a>
+			</div>    
+		</form>
+	</div>
+</div>
