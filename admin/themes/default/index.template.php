8ee7c234e16ade29171fda505657ad2850b2c7ab
EuanMead <euan.irvin.mead@gmail.com>
2014-01-07 16:33:37 +0000

    Use CDNJS For Hosting Scripts.

--- admin/themes/default/index.template.php
+++ admin/themes/default/index.template.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Monstra :: <?php echo __('Administration', 'system'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Monstra admin area" />
+    <meta name="description" content="Monstra Admin Mrea" />
     <link rel="icon" href="<?php echo Option::get('siteurl'); ?>/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="<?php echo Option::get('siteurl'); ?>/favicon.ico" type="image/x-icon" />
 
@@ -49,8 +49,8 @@
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
     <![endif]-->
   </head>
 
