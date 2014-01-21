aaba7cef27eaeee2dc574f1ab87f91d60b5abbae
Sergey Romanenko <awilum@msn.com>
2014-01-21 14:37:32 +0200

    Email Templates Plugin - Updates #2

---
+++ plugins/box/emails/views/backend/edit.view.php
@@ -0,0 +1,32 @@
+<h2><?php echo __('Edit Email Template', 'emails'); ?></h2>
+<br>
+<?php   
+if ($email_template_content !== null) {
+    echo (Form::open());
+    echo (Form::hidden('csrf', Security::token()));
+    echo (Form::hidden('email_template_name', Request::get('filename')));
+?>
+<?php echo (Form::label('name', __('Name', 'emails'))); ?>
+<div class="input-group">
+    <?php echo (Form::input('name', Request::get('filename'), array('disabled', 'class' => 'form-control'))); ?><span class="input-group-addon">.emails.php</span>
+</div>
+<br>
+<?php
+
+    echo (        
+       Form::label('email_template_content', __('Email template content', 'emails')).
+       Form::textarea('email_template_content', Html::toText($email_template_content), array('style' => 'width:100%;height:400px;', 'class' => 'source-editor form-control'))
+    );
+
+    echo (
+       Html::br().
+       Form::submit('edit_email_template_and_exit', __('Save and Exit', 'emails'), array('class' => 'btn btn-primary')).Html::nbsp(2).
+       Form::submit('edit_email_template', __('Save', 'emails'), array('class' => 'btn btn-primary')). Html::nbsp(2).
+       Html::anchor(__('Cancel', 'blocks'), 'index.php?id=emails', array('title' => __('Cancel', 'emails'), 'class' => 'btn btn-default')).
+       Form::close()
+    );
+
+} else {
+    echo '<div class="message-error">'.__('This email template does not exist', 'emails').'</div>';
+}
+?>
