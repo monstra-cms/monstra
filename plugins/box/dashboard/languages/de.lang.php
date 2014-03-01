253cb63630f3e2e82c11dbca5d6e09ff71905e6b
bernte <bernte@gmail.com>
2014-03-01 11:52:01 +0100

    Update de.lang.php

--- plugins/box/dashboard/languages/de.lang.php
+++ plugins/box/dashboard/languages/de.lang.php
@@ -5,7 +5,7 @@
             'Dashboard' => 'Dashboard',
             'Dashboard plugin for Monstra' => 'Dashboard plugin für Monstra',
             'Welcome back' => 'Willkommen zurück',
-            'Create New' => 'Erstelle neue(s)',
+            'Create New' => 'Erstelle neue',
             'Upload File' => 'Datei hochladen',
         )
     );
