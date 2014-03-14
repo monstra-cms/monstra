efb026013efced1421c9731191e7adc7f0707fc7
Awilum <awilum@msn.com>
2014-03-14 20:09:09 +0400

    Chinese Language

---
+++ plugins/box/snippets/languages/zh-cn.lang.php
@@ -0,0 +1,29 @@
+<?php
+
+    return array(
+        'snippets' => array(
+            'Snippets' => '片段',
+            'Snippets manager' => '片段管理',
+            'Actions' => '操作',
+            'Delete' => '删除',
+            'Edit' => '编辑',
+            'Name' => '名称',
+            'Create New Snippet' => '创建新片段',
+            'New Snippet' => '新建片段',
+            'Edit Snippet' => '编辑片段',
+            'Save' => '保存',
+            'Save and Exit' => '保存并退出',
+            'This snippet already exists' => '此片段已存在',
+            'This snippet does not exist' => '此片段不存在',
+            'Delete snippet: :snippet' => '删除片段: :snippet',
+            'Snippet content' => '片段内容',
+            'Snippet <i>:name</i> deleted' => '片段 <i>:name</i> 已删除',
+            'Your changes to the snippet <i>:name</i> have been saved.' => 'Y您修改的片段 <i>:name</i> 已保存。',
+            'Delete snippet: :snippet' => '删除片段: :snippet',
+            'Required field' => '必填字段',
+            'View Embed Code' => '查看嵌入代码',
+            'Embed Code' => '嵌入代码',
+            'Shortcode' => '短代码',
+            'PHP Code' => 'PHP 代码',
+        )
+    );
