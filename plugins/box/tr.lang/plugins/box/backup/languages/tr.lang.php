6a7c3a34625870d3ff69f938a53155cded017948
Awilum <awilum@msn.com>
2014-03-27 23:45:33 +0400

    Turkish Language

---
+++ plugins/box/tr.lang/plugins/box/backup/languages/tr.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    return array(
+        'backup' => array(
+    		'Backups' => 'Yedekler',
+            'Backup Date' => 'Yedek tarihi',
+    		'Create Backup' => 'Yedek Oluştur',
+    		'Delete' => 'Sil',
+            'storage' => 'Depolama',
+            'public' => 'Genel',
+            'plugins' => 'Eklentiler',
+            'Size' => 'Boyut',
+            'Actions' => 'Eylemler',
+            'Delete backup: :backup' => 'Yedek silindi: :backup',
+            'Creating...' => 'Oluşturuluyor...',
+        )
+	);
---
+++ plugins/box/tr.lang/plugins/box/menu/languages/tr.lang.php
@@ -0,0 +1,27 @@
+<?php
+
+    return array(
+        'menu' => array(
+            'Menu' => 'Menü',
+            'Menu manager' => 'Menu Yöneticisi',
+            'Edit' => 'Düzenle',
+            'Name' => 'İsim',
+            'Delete' => 'Sil',
+            'Order' => 'Sıralama',
+            'Actions' => 'İşlem',
+            'Create New Item' => 'Yeni Oluştur',
+            'New item' => 'Yeni Oluştur',
+            'Item name' => 'Menü Adı',
+            'Item order' => 'Sıralaması',
+            'Item target' => 'Hedef',
+            'Item link' => 'Bağlantısı',
+            'Item category' => 'Kategorisi',
+            'Save' => 'Kaydet',
+            'Edit item' => 'Düzenle',
+            'Delete item :name' => 'Sil :name',
+            'Select page' => 'Sayfa Seç',
+            'Category' => 'Kategori',
+            'Select category' => 'Kategori Seç',
+            'Required field' => 'Zorunlu Alan',
+        )
+    );
