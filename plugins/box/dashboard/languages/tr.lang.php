fb5e27807395107320f7e6270704ad32fc41aca3
Ali Galip Çamlı <Kibele@users.noreply.github.com>
2014-05-23 10:14:05 +0300

    Create tr.lang.php in box/dashboard plugin

---
+++ plugins/box/dashboard/languages/tr.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+    return array(
+        'dashboard' => array(
+            'Dashboard' => 'Pano',
+            'Dashboard plugin for Monstra' => 'Monstra için pano eklentisi',
+            'Welcome back' => 'Yeniden hoş geldiniz',
+            'Create New' => 'Yeni Oluştur',
+            'Upload File' => 'Dosya Yükle',
+        )
+    );
