ff18aba1b59d42fdf0078eff3ba931731c9c22ed
Ali Galip Çamlı <Kibele@users.noreply.github.com>
2014-05-24 11:00:40 +0300

    Create tr.lang.php in sandbox plugin

---
+++ plugins/sandbox/languages/tr.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+    return array(
+        'sandbox' => array(
+            'Sandbox' => 'Deneme',
+            'Sandbox plugin for Monstra' => 'Monstra için deneme/test eklentisi',
+            'Sandbox template' => 'Deneme şablonu',
+            'Save' => 'Kaydet',
+        )
+    );
