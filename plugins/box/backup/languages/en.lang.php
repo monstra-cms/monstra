45f6c4952e7b4a1ffbf22894e9346e4abc0a46f4
Ali Galip Çamlı <Kibele@users.noreply.github.com>
2014-05-24 17:34:28 +0300

    Update en.lang.php in box/backup plugin
    Some missed entries...

--- plugins/box/backup/languages/en.lang.php
+++ plugins/box/backup/languages/en.lang.php
@@ -15,5 +15,12 @@
             'Actions' => 'Actions',
             'Delete backup: :backup' => 'Delete backup: :backup',
             'Creating...' => 'Creating...',
+            'Backup was created' => 'Backup was created',
+            'Backup was not created' => 'Backup was not created',
+            'Backup was deleted' => 'Backup was deleted',
+            'Backup was not deleted' => 'Backup was not deleted',
+            'Backup was restored' => 'Backup was restored',
+            'Unzip error' => 'Unzip error',
+            'Backup was not restored' => 'Backup was not restored',
         )
 	);
