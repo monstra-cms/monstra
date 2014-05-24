b9408b3522101805cc5837b6ba2e5579cb44c9cf
Ali Galip Çamlı <Kibele@users.noreply.github.com>
2014-05-24 17:59:25 +0300

    Update en.lang.php in box/filesmanager plugin
    Some missed entries...

--- plugins/box/filesmanager/languages/en.lang.php
+++ plugins/box/filesmanager/languages/en.lang.php
@@ -16,5 +16,18 @@
             'Size' => 'Size',
             'Select file' => 'Select file',
             'Change' => 'Change',
+            'File was deleted' => 'File was deleted',
+            'File was not deleted' => 'File was not deleted',
+            'Directory was deleted' => 'Directory was deleted',
+            'Directory was not deleted' => 'Directory was not deleted',
+            'Can not be empty' => 'Can not be empty',
+            'Directory exists' => 'Directory exists',
+            'File exists' => 'File exists',
+            'Renamed successfully' => 'Renamed successfully',
+            'Failure' => 'Failure',
+            'File was uploaded' => 'File was uploaded',
+            'Directory was not created' => 'Directory was not created',
+            'Directory was created' => 'Directory was created',
+            'Upload server error' => 'Upload server error',
         )
     );
