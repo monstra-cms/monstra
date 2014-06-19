43655828c9041fb440c3d0417fdf4944e1258205
Sergey Romanenko <awilum@msn.com>
2014-06-19 13:31:23 +0400

    Update layout.email.php

--- storage/emails/layout.email.php
+++ storage/emails/layout.email.php
@@ -17,40 +17,40 @@
       }
     </style>
   </head>
-  <body>   
-    <table class="body">
-      <tr>
-        <td class="center" align="center" valign="top">
-          <center>
-            <table class="container">
-              <tr>
-                <td>
-                  <table class="row">
-                    <tr>
-                      <td class="wrapper">
-                        <table class="twelve columns">
-                          <tr>
-                            <td class="email-header" style="padding:10px 20px 10px 0px;">
-                              <h1><?php echo $site_name; ?></h1>
+  <body style="width: 100% !important; min-width: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 19px; font-size: 14px; margin: 0; padding: 0;">   
+    <table class="body" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; height: 100%; width: 100%; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;">
+      <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+        <td class="center" align="center" valign="top" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: center; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;">
+          <center style="width: 100%; min-width: 580px;">
+            <table class="container" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: inherit; width: 580px; margin: 0 auto; padding: 0;">
+              <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+                <td style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;" align="left" valign="top">
+                  <table class="row" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 100%; position: relative; display: block; padding: 0px;">
+                    <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+                      <td class="wrapper" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; position: relative; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 10px 20px 0px 0px;" align="left" valign="top">
+                        <table class="twelve columns" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 580px; margin: 0 auto; padding: 0;">
+                          <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+                            <td class="email-header" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 10px 20px 10px 0px;" align="left" valign="top">
+                              <h1 style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 1.3; word-break: normal; font-size: 40px; margin: 0; padding: 0;" align="left"><?php echo $site_name; ?></h1>
                             </td>
-                            <td class="expander"></td>
+                            <td class="expander" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; visibility: hidden; width: 0px; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;" align="left" valign="top"></td>
                           </tr>
                         </table>
                       </td>
                     </tr>
                   </table>
-                  <table class="row">
-                    <tr>
-                      <td class="wrapper">
-                        <table class="twelve columns">
-                          <tr>
-                            <td class="email-content" style="border:1px solid #F5F5F5; background:#fdfdfd; padding:20px 20px 20px 20px;">
-                              <?php include STORAGE . DS . 'emails' . DS . $email_template . '.email.php'; ?>
-                            </td>
-                            <td class="expander"></td>
-                          </tr>
+                  <table class="row" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 100%; position: relative; display: block; padding: 0px;">
+                    <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+                      <td class="wrapper" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; position: relative; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 10px 20px 0px 0px;" align="left" valign="top">
+                        <table class="twelve columns" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 580px; margin: 0 auto; padding: 0;">
+                        <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
+                          <td class="email-content" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; background: #fdfdfd; margin: 0; padding: 20px; border: 1px solid #f5f5f5;" align="left" bgcolor="#fdfdfd" valign="top">
+                            <?php include STORAGE . DS . 'emails' . DS . $email_template . '.email.php'; ?>
+                          </td>
+                          <td class="expander" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; visibility: hidden; width: 0px; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;" align="left" valign="top"></td>
+                        </tr>
                         </table>
-                      </td>
+                        </td>
                     </tr>
                   </table>
                 </td>
