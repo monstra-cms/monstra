268237e0c2db10792901656800664d6896b792e1
Freddy Morán Jr. <Freddynic159@users.noreply.github.com>
2014-09-16 09:50:34 -0600

    Create es.lang.php

---
+++ plugins/box/emails/languages/es.lang.php
@@ -0,0 +1,19 @@
+<?php
+
+    return array(
+        'emails' => array(
+            'Emails' => 'Emails',
+            'Emails plugin for Monstra' => 'Emails plugin para Monstra',
+            'Edit Layout' => 'Editar Layout',
+            'Email templates' => 'Plantillas de Email',
+            'Edit' => 'Editar',
+            'Edit Email Template' => 'Editar Plantilla de Email',
+            'Name' => 'Nombre',
+            'Email template content' => 'Contenido de la plantilla de email',
+            'Save and Exit' => 'Guardar y Salir',
+            'Save' => 'Guardar',
+            'Cancel' => 'Cancelar',
+            'This email template does not exist' => 'This email template does not exist',
+            'Your changes to the email template <i>:name</i> have been saved.' => 'Tus cambios en la plantilla de email <i>:name</i> han sido guardados.',
+        )
+    );
