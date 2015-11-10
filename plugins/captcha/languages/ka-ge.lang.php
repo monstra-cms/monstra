b526ee2d4dd8515a95d24b8df6a6782b159c3c28
mapadesign <panic.marjan@gmail.com>
2015-11-10 20:24:53 +0100

    Create ka-ge.lang.php

---
+++ plugins/captcha/languages/ka-ge.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+        'captcha' => array(
+            'Captcha' => 'Captcha',
+            'Captcha plugin for Monstra' => 'Captcha პლაგინი Monstra-თვის',
+            'Captcha code is wrong' => 'Captcha-ზე პასუხი არასწორია!',
+        )
+    );
