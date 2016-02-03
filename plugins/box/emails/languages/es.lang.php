86af3e210921749c7c135496a37cf64a79461edb
Freddy Morán Jr <Freddynic159@users.noreply.github.com>
2016-02-03 10:24:45 -0600

    Update es.lang.php

--- plugins/box/emails/languages/es.lang.php
+++ plugins/box/emails/languages/es.lang.php
@@ -3,7 +3,7 @@
     return array(
         'emails' => array(
             'Emails' => 'Emails',
-            'Emails plugin for Monstra' => 'Emails plugin para Monstra',
+            'Emails plugin for Monstra' => 'Emails plugin for Monstra',
             'Edit Layout' => 'Editar diseño',
             'Email templates' => 'Plantillas de email',
             'Edit' => 'Editar',
@@ -14,6 +14,6 @@
             'Save' => 'Guardar',
             'Cancel' => 'Cancelar',
             'This email template does not exist' => 'Esta plantilla de email no existe',
-            'Your changes to the email template <i>:name</i> have been saved.' => 'Sus cambios en la plantilla de email <i>:name</i> han sido guardados.',
+            'Your changes to the email template <i>:name</i> have been saved.' => 'Tus cambios en la plantilla de email <i>:name</i> han sido guardados.',
         )
     );
